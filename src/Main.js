938764de354b7e9182b5ac305221d9614068c61a
sliganslogan <alistairfink@gmail.com>
2019-08-05 04:13:32 -0400

    fix errors

--- src/Main.js
+++ src/Main.js
@@ -62,16 +62,18 @@ function Main() {
       <div className="Link-Table">
         <h3>Links</h3>
         <table>
-          <tr className="Link-Table-Headers">
-            <td>Shortened Url</td>
-            <td>Link</td>
-          </tr>
-          {links.map((item) => 
-            <tr>
-              <td className="Link-Table-Left">{window.location.href + item.Id}</td>
-              <td className="Link-Table-Right">{item.Link}</td>
+          <tbody>
+            <tr className="Link-Table-Headers">
+              <td>Shortened Url</td>
+              <td>Link</td>
             </tr>
-          )}
+            {links.map((item) => 
+              <tr key={item.Id }>
+                <td className="Link-Table-Left">{window.location.href + item.Id}</td>
+                <td className="Link-Table-Right">{item.Link}</td>
+              </tr>
+            )}
+          </tbody>
         </table>
       </div>
     </div>
